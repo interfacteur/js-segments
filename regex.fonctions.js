470dc02ab5e73803b0a28f02503b936c56c62227
Interfacteur <gaetan@interfacteur.net>
2016-01-19 21:14:51 +0100

    correction

--- regex.fonctions.js
+++ regex.fonctions.js
@@ -3,7 +3,7 @@ var cleanJSON = function (str) {
 	return str.replace(/\/\/.*/g, "") //commentaires sur une ligne
 		.replace(/\/\*([^\*]|\*(?!\/))*\*\//g, "") //commentaires sur plusieurs lignes
 		.replace(/^\s*/, "") //espacements initiaux
-		.replace(/\s\s/g, "\s") //espacements doubles
+		.replace(/\s\s/g, " ") //espacements doubles
 		.replace(/[\n\r\u2028\u2029\t]/g, ""); //retours à la ligne et charriot, tabulations
 }
 	/* usage initial : https://github.com/interfacteur/divers/blob/master/proxies-apis/interfaces-tests/cross-avec-jquery.xdomainrequest.html */
@@ -14,6 +14,6 @@ var clearJS = function (str) {
 		.replace(/\/\*([^\*]|\*(?!\/))*\*\//g, "") //commentaires sur plusieurs lignes
 		.replace(/[\t]/g, " ") //tabulations par un espacement
 		.replace(/^\s*/, "") //espacements initiaux
-		.replace(/\s\s/g, "\s"); //espacements doubles
+		.replace(/\s\s/g, " "); //espacements doubles
 }
 
