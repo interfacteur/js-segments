953877b6b1bb6d1098aa1627c5d1ac1a2442911c
Interfacteur <gaetan@interfacteur.net>
2016-01-20 00:15:05 +0100

    corrections and new functions

--- regex.fonctions.js
+++ regex.fonctions.js
@@ -1,19 +1,34 @@
-//Nettoyer le contenu d'un fichier JSON
+//Nettoyer un contenu JSON et le mettre sur une ligne
 var cleanJSON = function (str) {
-	return str.replace(/\/\/.*/g, "") //commentaires sur une ligne
-		.replace(/\/\*([^\*]|\*(?!\/))*\*\//g, "") //commentaires sur plusieurs lignes
-		.replace(/^\s*/, "") //espacements initiaux (dont retours)
-		.replace(/\s\s+/g, " ") //espacements multiples (dont retours) par un espacement
-		.replace(/[\f\n\r\u2028\u2029\t\v]/g, ""); //retours à la page, à la ligne et charriot, tabulations horizontales et verticales
+	return str.replace(/\/\/.*/g, "") //sans les commentaires sur une ligne
+		.replace(/\/\*([^\*]|\*(?!\/))*\*\//g, "") //sans les commentaires sur plusieurs lignes
+		.replace(/^\s*/, "") //sans les espacements initiaux (dont retours)
+		.replace(/\s\s+/g, " ") //remplacer les espacements multiples (dont retours) par un espacement
+		.replace(/[\f\n\r\u2028\u2029\t\v]/g, ""); //sans les retours à la page, à la ligne et charriot, tabulations horizontales et verticales
 }
 	/* usage initial : https://github.com/interfacteur/divers/blob/master/proxies-apis/interfaces-tests/cross-avec-jquery.xdomainrequest.html */
 
-//Éclaircir le contenu d'un fichier .js
+//Éclaircir un contenu JavaScript
 var clearJS = function (str) {
-	return str.replace(/\/\/.*/g, "") //commentaires sur une ligne
-		.replace(/\/\*([^\*]|\*(?!\/))*\*\//g, "") //commentaires sur plusieurs lignes
-		.replace(/[\t\v]/g, " ") //tabulations horizontales et verticales par un espacement
-		.replace(/^\s*/, "") //espacements initiaux (dont retours)
-		.replace(/\x20\x20+/g, " "); //espacements multiples par un espacement
+	return str.replace(/\/\/.*/g, "") //sans les commentaires sur une ligne
+		.replace(/\/\*([^\*]|\*(?!\/))*\*\//g, "") //sans les commentaires sur plusieurs lignes
+		.replace(/^\s*/, "") //sans les espacements initiaux (dont retours)
+		.replace(/[\t\v]/g, " ") //remplacer les tabulations horizontales et verticales par un espacement
+		.replace(/\x20{2,}/g, " "); //remplacer les espacements multiples par un espacement
+}
+
+//Éclaircir un contenu JavaScript et le mettre sur une ligne
+var clearLineJS = function (str) {
+	return str.replace(/\/\/.*/g, "") //sans les commentaires sur une ligne
+		.replace(/\/\*([^\*]|\*(?!\/))*\*\//g, "") //sans les commentaires sur plusieurs lignes
+		.replace(/^\s*/, "") //sans les espacements initiaux (dont retours)
+		.replace(/[\t\v]/g, " ") //remplacer les tabulations horizontales et verticales par un espacement
+		.replace(/\x20{2,}/g, " ") //remplacer les espacements multiples par un espacement
+		.replace(/[\f\n\r\u2028\u2029]/g, ""); //sans les retours à la page, à la ligne et charriot
+}
+
+//Mettre sur une ligne
+var uniLine = function (str) {
+	return str.replace(/[\f\n\r\u2028\u2029]/g, ""); //sans les retours à la page, à la ligne et charriot
 }
 
