f6e491fbe2dbee3e0c84bb3461303e394ef1b9fd
Interfacteur <gaetan@interfacteur.net>
2016-01-19 21:39:04 +0100

    corrections

--- regex.fonctions.js
+++ regex.fonctions.js
@@ -2,9 +2,9 @@
 var cleanJSON = function (str) {
 	return str.replace(/\/\/.*/g, "") //commentaires sur une ligne
 		.replace(/\/\*([^\*]|\*(?!\/))*\*\//g, "") //commentaires sur plusieurs lignes
-		.replace(/^\s*/, "") //espacements initiaux
-		.replace(/\s\s/g, " ") //espacements doubles
-		.replace(/[\n\r\u2028\u2029\t]/g, ""); //retours à la ligne et charriot, tabulations
+		.replace(/^\s*/, "") //espacements initiaux (dont retours)
+		.replace(/\s\s+/g, " ") //espacements multiples (dont retours) par un espacement
+		.replace(/[\f\n\r\u2028\u2029\t\v]/g, ""); //retours à la page, à la ligne et charriot, tabulations horizontales et verticales
 }
 	/* usage initial : https://github.com/interfacteur/divers/blob/master/proxies-apis/interfaces-tests/cross-avec-jquery.xdomainrequest.html */
 
@@ -12,8 +12,8 @@ var cleanJSON = function (str) {
 var clearJS = function (str) {
 	return str.replace(/\/\/.*/g, "") //commentaires sur une ligne
 		.replace(/\/\*([^\*]|\*(?!\/))*\*\//g, "") //commentaires sur plusieurs lignes
-		.replace(/[\t]/g, " ") //tabulations par un espacement
-		.replace(/^\s*/, "") //espacements initiaux
-		.replace(/\s\s/g, " "); //espacements doubles
+		.replace(/[\t\v]/g, " ") //tabulations horizontales et verticales par un espacement
+		.replace(/^\s*/, "") //espacements initiaux (dont retours)
+		.replace(/\x20\x20+/g, " "); //espacements multiples par un espacement
 }
 
