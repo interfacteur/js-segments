58b0e5c10f663ac9fac19ae5c69fbf5ce9c82c38
Interfacteur <gaetan@interfacteur.net>
2016-01-25 20:30:00 +0100

    to end with multiples spacings

--- regex.fonctions.js
+++ regex.fonctions.js
@@ -3,8 +3,8 @@ var cleanJSON = function (str) {
 	return str.replace(/\/\/.*/g, "") //sans les commentaires sur une ligne
 		.replace(/\/\*([^\*]|\*(?!\/))*\*\//g, "") //sans les commentaires sur plusieurs lignes
 		.replace(/^\s*/, "") //sans les espacements initiaux (dont retours)
-		.replace(/\s\s+/g, " ") //remplacer les espacements multiples (dont retours) par un espacement
-		.replace(/[\f\n\r\u2028\u2029\t\v]/g, ""); //sans les retours à la page, à la ligne et charriot, tabulations horizontales et verticales
+		.replace(/[\f\n\r\u2028\u2029\t\v]/g, "") //sans les retours à la page, à la ligne et charriot, tabulations horizontales et verticales
+		.replace(/\s\s+/g, " "); //remplacer les espacements multiples (dont retours) par un espacement
 }
 	/* usage initial : https://github.com/interfacteur/divers/blob/master/proxies-apis/interfaces-tests/cross-avec-jquery.xdomainrequest.html */
 
@@ -23,8 +23,8 @@ var clearLineJS = function (str) {
 		.replace(/\/\*([^\*]|\*(?!\/))*\*\//g, "") //sans les commentaires sur plusieurs lignes
 		.replace(/^\s*/, "") //sans les espacements initiaux (dont retours)
 		.replace(/[\t\v]/g, " ") //remplacer les tabulations horizontales et verticales par un espacement
-		.replace(/\x20{2,}/g, " ") //remplacer les espacements multiples par un espacement
-		.replace(/[\f\n\r\u2028\u2029]/g, ""); //sans les retours à la page, à la ligne et charriot
+		.replace(/[\f\n\r\u2028\u2029]/g, "") //sans les retours à la page, à la ligne et charriot
+		.replace(/\x20{2,}/g, " "); //remplacer les espacements multiples par un espacement
 }
 
 //Mettre sur une ligne
