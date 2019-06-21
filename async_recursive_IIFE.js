c9b0bf34ca72e62acf4b2a6a8bbb8e52808ae8e7
dépôts, archives <interfacteur@users.noreply.github.com>
2019-06-21 17:31:23 +0200

    Update async_recursive_IIFE.js

--- async_recursive_IIFE.js
+++ async_recursive_IIFE.js
@@ -25,3 +25,42 @@
   après lecture de
   https://stackoverflow.com/questions/42964102/syntax-for-async-arrow-function/42964310#42964310
 */
+
+
+//EXEMPLE DANS LE PROJET WebForce3 :
+    //déterminer la hauteur de l'élément : pour les placeholder à hauteurs distinces du drag and drop (jQuery UI)
+        // - IIFE fléchée contenant recursive async. (donc promesse chainable) : initialement pour insertion dynamique de l'élément dont mesurer hauteur,
+        // conservé ici en cas de scope hors ready de jQuery
+//déterminer la hauteur de l'élément : pour les placeholder à hauteurs distinces du drag and drop (jQuery UI)
+// - IIFE fléchée contenant recursive async. (donc promesse chainable) : initialement pour insertion dynamique de l'élément dont mesurer hauteur,
+// conservé ici en cas de scope hors ready de jQuery
+(() =>
+{
+    let height;
+    const heigthWatch = async $elem =>
+    {
+        let elemHeight;
+        await new Promise(resolve =>
+            setTimeout(resolve, 100)
+        );
+        elemHeight = $elem.outerHeight();
+        return elemHeight > 2 ? Math.round(elemHeight) : heigthWatch();
+    };
+    heigthWatch($('.calibration1'))
+        .then(h =>
+        {
+            height = h;
+            return heigthWatch($('.calibration2'));
+        })
+        .then(h =>
+        {
+            $('.calibration').remove();
+            $('html > head').append(
+                $(`<style>
+                    .episode_placeholder:nth-child(-n+2) { height: ${height}px; }
+                    .episode_placeholder { height: ${h}px; }
+                 </style>`)
+            );
+            $body.removeClass('placeholderCalculate');
+        });
+})();
